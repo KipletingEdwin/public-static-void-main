6b9a9c6ffaa6cbb9b224bafbe448f13624b588e3
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-10-09 21:29:10 +0300

    add an array

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        String [] ex = new String[] {"hey", "cool", "arr"};
+
+        System.out.println(ex[0]);
+        System.out.println(ex[1]);
+        System.out.println(ex[2]);
 
     }
 }
